0a24f0e2c86c96d687f0b5b0dc05424c42b79313
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-27 10:16:24 +0200

    Solve

--- challenge.js
+++ challenge.js
@@ -1,32 +1,17 @@
 function solution(string) {
-  let count = 0;
-  const first = 0;
-  let second = 1;
   let piece;
 
-  function recursive(string, first, second) {
-    for (second; second < string.length; second++) {
-      if (string[second] === string[first]) {
-        count++
-        piece = string.slice(0, second);
-        break;
+  for (let i = 1; i < string.length; i++) {
+
+    if (string[i] === string[0]) {
+      piece = string.slice(0, i);
+      const regExp = new RegExp(piece, "g");
+      const match = string.match(regExp);
+
+      if (match.join("") === string) {
+        return match.length;
       }
     }
-
-    const regExp = new RegExp(piece, "g")
-
-    count = string.match(regExp).length
-
-    console.log(string.match(regExp))
-
-    if (count === 0) {
-      recursive(string, first, second+1)
-    }
   }
-
-  recursive(string, first, second)
-  return count
+  return 0
 }
-
-console.log(solution("abcabcabcabc"));
-// console.log(solution("abccbaabccba"));
